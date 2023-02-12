033559ba68ac48011c91d402294cadb67e5cfc84
Stasiassi <simonovanastasia360@gmail.com>
2023-02-12 18:10:19 +0300

    feat: make morse decoder

--- src/index.js
+++ src/index.js
@@ -35,10 +35,49 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' '    :  ' ',
 };
 
 function decode(expr) {
     // write your solution here
+    let array = [];
+    let morse ="";
+    let matrix =[];
+
+    for(let i = 0 ; i < expr.length; i += 10)
+    {
+        array.push(expr.slice(i, i + 10));
+    }
+
+    for(let i = 0; i < array.length; i++)
+    {
+        morse = '';
+        if(array[i] == '**********') 
+        {
+            morse += " ";
+        }
+        else
+        {
+            for(let j = 0; j < 11; j += 2)
+            {
+                if (array[i][j] == '1' && array[i][j+1] == '0')
+                {
+                    morse += ".";
+                }
+                else if (array[i][j] == '1' && array[i][j+1] == '1')
+                {
+                    morse += "-";
+                }
+            }
+        }
+        matrix.push(morse);
+    }
+    morse = '';
+    for(let i = 0; i < matrix.length; i++)
+    {
+        morse += MORSE_TABLE[matrix[i]];
+    }
+    return morse;
 }
 
 module.exports = {
